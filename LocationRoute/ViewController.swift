3291bb5d34fb8b4c7eff5a61c239ccdb9bf41327
ArtyomPotapov <artyom.potapov@icloud.com>
2022-11-06 22:43:52 +0300

    Установка карты, кнопок, констрейнтов, таргетов кнопок.

--- LocationRoute/ViewController.swift
+++ LocationRoute/ViewController.swift
@@ -6,14 +6,93 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+    lazy var setNewAddressButton: UIButton = setButton(systemName: "plus.square.fill.on.square.fill")
+    
+    lazy var buildRouteButton: UIButton = setButton(systemName: "arrowshape.bounce.forward.fill")
+    
+    lazy var deleteAllAddressesButton: UIButton = setButton(systemName: "multiply.square.fill")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setViews()
+        addTargetsToButtons()
+        setConstraints()
     }
 
+    func setButton(systemName: String) -> UIButton {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 8
+        button.clipsToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 35)
+        button.tintColor = UIColor(named: "darkRed")
+        button.setImage(UIImage(systemName: systemName, withConfiguration: imageConfig ), for: .normal)
+        return button
+    }
+    
+    func setViews(){
+        view.addSubview(mapView)
+        mapView.addSubview(setNewAddressButton)
+        mapView.addSubview(buildRouteButton)
+        mapView.addSubview(deleteAllAddressesButton)
+    }
+    
+    func addTargetsToButtons(){
+        setNewAddressButton.addTarget(self, action: #selector(setNewAddressButtonTapped), for: .touchUpInside)
+        buildRouteButton.addTarget(self, action: #selector(buildRouteButtonTapped), for: .touchUpInside)
+        deleteAllAddressesButton.addTarget(self, action: #selector(deleteAllAddressButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func setNewAddressButtonTapped(){
+        
+    }
+    
+    @objc func buildRouteButtonTapped(){
+        
+    }
+    
+    @objc func deleteAllAddressButtonTapped(){
+        
+    }
+    
+    
+    func setConstraints(){
+        
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            setNewAddressButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 20),
+            setNewAddressButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -10),
+            setNewAddressButton.heightAnchor.constraint(equalToConstant: 50),
+            setNewAddressButton.widthAnchor.constraint(equalToConstant: 50),
+            
+            buildRouteButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 90),
+            buildRouteButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -10),
+            buildRouteButton.heightAnchor.constraint(equalToConstant: 50),
+            buildRouteButton.widthAnchor.constraint(equalToConstant: 50),
+            
+            deleteAllAddressesButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 160),
+            deleteAllAddressesButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -10),
+            deleteAllAddressesButton.heightAnchor.constraint(equalToConstant: 50),
+            deleteAllAddressesButton.widthAnchor.constraint(equalToConstant: 50)
+            
+        ])
+    }
 
 }
 
